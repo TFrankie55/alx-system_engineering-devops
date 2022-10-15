7cc51a1d67be6ef7e0ee9166ebaaf239bfabbae6
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-10-15 01:34:32 +0100

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
-# creates a manifest that kills a process
-
+# creates a manifest that kills a process.
 exec {
-'pkill -f killmenow':
-path => 'usr/bin:usr/local/bin:bin'
+  'pkill -f killmenow':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
