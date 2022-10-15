4875cad67d866f9706ff5ffdb12aac59348e0cb1
TFrankie55 <towoeye50@gmail.com>
2022-10-14 17:40:46 -0700

    killing a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 # creates a manifest that kills a process
 exec {
 'pkill -f killmenow':
-path => 'usr/bin:usr/local/bin:biin'
-=======
-# creates a manifest that kills a process.
-exec {
-  'pkill -f killmenow':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
->>>>>>> 7cc51a1d67be6ef7e0ee9166ebaaf239bfabbae6
+path => 'usr/bin:usr/local/bin:bin'
 }
